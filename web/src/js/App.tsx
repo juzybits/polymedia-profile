025b08e60e57d45ca39705d48c719a99e60d85b3
juzybits <96890594+juzybits@users.noreply.github.com>
2023-01-15 05:05:45 -0100

    [web] move profile.ts here for development

--- web/src/js/App.tsx
+++ web/src/js/App.tsx
@@ -24,54 +24,11 @@ export function App()
 }
 
 
-
-import { JsonRpcProvider, Network } from '@mysten/sui.js';
-import { BCS, getSuiMoveConfig } from '@mysten/bcs';
-
-const rpc = new JsonRpcProvider(Network.DEVNET);
-const bcs = new BCS(getSuiMoveConfig());
-
-export type GetProfilesArgs = {
-    packageId: string;
-    registryId: string;
-    lookupAddresses: string[];
-}
-export async function getProfiles({ packageId, registryId, lookupAddresses }
-        : GetProfilesArgs): Promise<Array<string>|string> {
-    const moveCall = {
-        packageObjectId: packageId,
-        module: 'profile',
-        function: 'get_profiles',
-        typeArguments: [],
-        arguments: [
-            registryId,
-            lookupAddresses,
-        ],
-    };
-    const callerAddress = '0x7777777777777777777777777777777777777777';
-    return await rpc.devInspectMoveCall(callerAddress, moveCall)
-    .then((resp: any) => {
-        const effects = resp.effects || resp.EffectsCert?.effects?.effects; // Sui/Ethos || Suiet
-        if (effects.status.status == 'success') {
-            const returnValue: Array<any> = resp.results.Ok[0][1].returnValues[0]; // grab the 1st and only tuple
-            const valueType: string = returnValue[1];
-            const valueData = Uint8Array.from(returnValue[0]);
-            const profileAddreses: Array<string> = bcs.de(valueType, valueData, 'hex');
-            return profileAddreses;
-        } else {
-            return effects.status.error;
-        }
-    })
-    .catch((error: any) => {
-        return error.message;
-    });
-}
-
+import { getProfileObjectIds } from './lib/profile';
 
 // TODO: remove
-// import { getProfiles } from '@polymedia/tools';
 async function foo() {
-    const result = await getProfiles({
+    const result = await getProfileObjectIds({
         packageId: '0xc7722a5f88c5cbf0f4005eed969e76bebeba2415',
         registryId: '0xd58562b32f7016c88dc351a92040cd5255ce8831',
         lookupAddresses: [
---
+++ web/src/js/lib/profile.ts
@@ -0,0 +1,97 @@
+import { JsonRpcProvider, Network, SignableTransaction } from '@mysten/sui.js';
+import { BCS, getSuiMoveConfig } from '@mysten/bcs';
+
+const rpc = new JsonRpcProvider(Network.DEVNET);
+const bcs = new BCS(getSuiMoveConfig());
+
+export type GetProfilesArgs = {
+    packageId: string;
+    registryId: string;
+    lookupAddresses: string[];
+}
+// TODO: getProfiles() : Promise<Array<Profile>>
+export async function getProfileObjectIds({ packageId, registryId, lookupAddresses }
+        : GetProfilesArgs): Promise<Array<string>|string> {
+    const moveCall = {
+        packageObjectId: packageId,
+        module: 'profile',
+        function: 'get_profiles',
+        typeArguments: [],
+        arguments: [
+            registryId,
+            lookupAddresses,
+        ],
+    };
+    const callerAddress = '0x7777777777777777777777777777777777777777';
+    return await rpc.devInspectMoveCall(callerAddress, moveCall)
+    .then((resp: any) => {
+        const effects = resp.effects || resp.EffectsCert?.effects?.effects; // Sui/Ethos || Suiet
+        if (effects.status.status == 'success') {
+            const returnValue: Array<any> = resp.results.Ok[0][1].returnValues[0]; // grab the 1st and only tuple
+            const valueType: string = returnValue[1];
+            const valueData = Uint8Array.from(returnValue[0]);
+            const profileAddreses: Array<string> = bcs.de(valueType, valueData, 'hex');
+            return profileAddreses;
+        } else {
+            return effects.status.error;
+        }
+    })
+    .catch((error: any) => {
+        return error.message;
+    });
+}
+
+export type WalletArg = {
+    signAndExecuteTransaction: (transaction: SignableTransaction) => Promise<any>,
+}
+export type CreateRegistryArgs = {
+    wallet: WalletArg,
+    packageId: string;
+    registryName: string;
+}
+export async function createRegistry({ wallet, packageId, registryName }
+        : CreateRegistryArgs): Promise<any> {
+    return wallet.signAndExecuteTransaction({
+        kind: 'moveCall',
+        data: {
+            packageObjectId: packageId,
+            module: 'profile',
+            function: 'create_registry',
+            typeArguments: [],
+            arguments: [
+                registryName,
+            ],
+            gasBudget: 1000,
+        }
+    });
+
+}
+
+export type CreateProfileArgs = {
+    wallet: WalletArg,
+    packageId: string;
+    registryId: string,
+    name: string,
+    image: string,
+    description: string,
+}
+export async function createProfile({ wallet, packageId, registryId, name, image, description }
+        : CreateProfileArgs): Promise<any> {
+    return wallet.signAndExecuteTransaction({
+        kind: 'moveCall',
+        data: {
+            packageObjectId: packageId,
+            module: 'profile',
+            function: 'create_profile',
+            typeArguments: [],
+            arguments: [
+                registryId,
+                name,
+                image,
+                description,
+            ],
+            gasBudget: 1000,
+        }
+    });
+
+}
