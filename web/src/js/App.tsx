94baf29fb55d343f9db9b6b80340ba911e16c455
juzybits <96890594+juzybits@users.noreply.github.com>
2023-01-16 22:54:01 -0100

    [web] remove dev code

--- web/src/js/App.tsx
+++ web/src/js/App.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { EthosConnectProvider } from 'ethos-connect';
 
@@ -7,11 +6,6 @@ import imgLogo from '../img/logo.png';
 
 export function App()
 {
-    // TODO: remove
-    useEffect(() => {
-        foo();
-    }, []);
-
     return <EthosConnectProvider
         ethosConfiguration={{hideEmailSignIn: true}}
         dappName='Polymedia Profile'
@@ -21,22 +15,4 @@ export function App()
         <Nav />
         <Outlet context={[]} />
     </EthosConnectProvider>;
-}
-
-
-import { getProfileObjectIds } from '@polymedia/profile-sdk';
-
-// TODO: remove
-async function foo() {
-    const result = await getProfileObjectIds({
-        packageId: '0x029a669d58113d3153811722f684dc3b7785543d',
-        registryId: '0x0faaf2d1fcb02a6a3a1372d8b149929051f7a84a',
-        lookupAddresses: [
-            '0x1111111111111111111111111111111111111111',
-            '0xe6717d39d166d9fa852c52e9ec0f76cb750457b7',
-            '0x2222222222222222222222222222222222222222',
-        ],
-    });
-    console.log(JSON.stringify(result));
-    console.log(result);
 }
