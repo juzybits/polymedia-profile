a7cfbcdbde8a9e05630146d65a8f8b8705a3ed7d
juzybits <96890594+juzybits@users.noreply.github.com>
2023-02-25 16:32:18 -0100

    [web] update to @mysten/sui.js@0.29.1

--- sdk/src/profile.ts
+++ sdk/src/profile.ts
@@ -1,11 +1,11 @@
 import { BCS, getSuiMoveConfig } from '@mysten/bcs';
 import {
     DevInspectResults,
+    devnetConnection,
     ExecuteTransactionRequestType,
     GetObjectDataResponse,
     JsonRpcProvider,
     MoveCallTransaction,
-    Network,
     OwnedObjectRef,
     SignableTransaction,
     SuiAddress,
@@ -16,11 +16,11 @@ import {
     UnserializedSignableTransaction,
 } from '@mysten/sui.js';
 
-const RPC_DEVNET = new JsonRpcProvider(Network.DEVNET);
+const RPC_DEVNET = new JsonRpcProvider(devnetConnection);
 export const POLYMEDIA_PROFILE_PACKAGE_ID_DEVNET = '0xd80bc36eef268d15f9888711fd6cd68d11fe6f59';
 export const POLYMEDIA_PROFILE_REGISTRY_ID_DEVNET = '0xaee180adbdd2cb389671ebfca93c89b841dad214';
 
-const RPC_TESTNET = new JsonRpcProvider('https://fullnode.testnet.sui.io:443');
+const RPC_TESTNET = new JsonRpcProvider(devnetConnection);
 export const POLYMEDIA_PROFILE_PACKAGE_ID_TESTNET = '0x123';
 export const POLYMEDIA_PROFILE_REGISTRY_ID_TESTNET = '0x123';
 
--- web/src/js/Home.tsx
+++ web/src/js/Home.tsx
@@ -11,7 +11,7 @@ export const Home: React.FC = () =>
             HOME
         </h1>
         <p>
-            Polymedia Profile is the on-chain profile/identity system that is used in all our projects, such as <a href='https://chat.polymedia.app/@sui-fans' target='_blank'>Polymedia Chat</a>.
+            Polymedia Profile is the on-chain profile/identity system that is used in all our apps, such as <a href='https://chat.polymedia.app/@sui-fans' target='_blank'>Polymedia Chat</a>.
             <br/>
             <br/>
             We will release a JavaScript SDK to make it really easy for others to integrate Polymedia Profile into their own projects.
