d5c213d8d3c00f1b462b52d4c2bb710a623bc927
juzybits <96890594+juzybits@users.noreply.github.com>
2023-08-07 03:12:06 +0000

    [web] /view/:profileId UI

--- web/src/js/ViewProfile.tsx
+++ web/src/js/ViewProfile.tsx
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom';
 import { useOutletContext } from 'react-router-dom';
 import { notifyError } from './components/Notification';
 import { useEffect, useState } from 'react';
+import { linkToExplorer } from '@polymedia/webutils';
 
 export const ViewProfile: React.FC = () =>
 {
@@ -13,6 +14,7 @@ export const ViewProfile: React.FC = () =>
     const profileId: string = useParams().profileId || '';
 
     const {
+        network,
         profileManager,
     } = useOutletContext<AppContext>();
 
@@ -47,11 +49,50 @@ export const ViewProfile: React.FC = () =>
         })
     };
 
+    let view: React.ReactNode;
+
+    if (profile === undefined) {
+        view = <>
+            Loading...
+        </>;
+    }
+    else if (profile === null) {
+        view = <>
+            Profile not found.
+        </>;
+    }
+    else {
+        view = <div id='profile-card'>
+            <div className='profile-card'>
+                <div className='profile-owner'>
+                    <h2>{profile.name}</h2>
+                    <div className='section'>
+                        <i>{profile.description ? profile.description : '(no description)'}</i>
+                    </div>
+                    <div className='section'>
+                        Profile: <a href={linkToExplorer(network, 'object', profile.id)}
+                            target='_blank' rel='noopener'> {shortenAddress(profile.id)}</a>
+                    </div>
+                    <div className='section'>
+                        Owner: <a href={linkToExplorer(network, 'address', profile.owner)}
+                            target='_blank' rel='noopener'> {shortenAddress(profile.owner)}</a>
+                    </div>
+                    <div className='section'>
+                        Registry: <a href={linkToExplorer(network, 'object', profileManager.registryId)}
+                            target='_blank' rel='noopener'>{shortenAddress(profileManager.registryId)}</a>
+                    </div>
+                    <div className='section profile-image'>
+                        {profile.imageUrl ? <img src={profile.imageUrl} /> : <i>(no image)</i>}
+                    </div>
+                </div>
+            </div>
+        </div>;
+    }
+
     return <div id='page' className='page-view-profile'>
-        TODO view profile
-        <br/>
-        <p style={{overflowWrap: 'anywhere'}}>
-            {JSON.stringify(profile, null, 4)}
-        </p>
+        {view}
     </div>;
+}
+function shortenAddress(address: string): string {
+    return '@' + address.slice(2, 10) + '...' + address.slice(-8);
 }
